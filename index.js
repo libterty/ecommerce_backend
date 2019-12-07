9abd5137d1ec1133115ded2d8a69102a7a1b6cc5
dreak603 <dreak603@gmail.com>
2019-12-07 14:03:54 +0800

    project init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+// const passport = require('./config/passport');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+app.use(cors({ credentials: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.use(methodOverride('_method'));
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening on port http://localhost:${port}!`)
+);
+
+module.exports = app;
