e72f70d35b3e4e5a7bfb85ae5528f143ecd93ea2
dreak603 <dreak603@gmail.com>
2019-12-07 22:02:18 +0800

    setUp CI

---
+++ test/server.test.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const supertest = require('supertest');
+
+const app = require('../index');
+
+describe('User', function() {
+  let request = null;
+  let server = null;
+
+  before(function(done) {
+    server = app.listen(done);
+    request = supertest.agent(server);
+  });
+
+  after(function(done) {
+    server.close(done);
+  });
+  it('should return a 200 response', function(done) {
+    request
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect(200, done);
+  });
+});
