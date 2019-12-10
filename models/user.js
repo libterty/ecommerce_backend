34c1e68a1c93dbffe657a86d994d1fceb9b7a59d
dreak603 <dreak603@gmail.com>
2019-12-10 17:27:43 +0800

    create temp migrations and user model for testing CD

---
+++ models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      address: DataTypes.STRING,
+      role: DataTypes.STRING
+    },
+    {}
+  );
+  User.associate = function(models) {};
+  return User;
+};
