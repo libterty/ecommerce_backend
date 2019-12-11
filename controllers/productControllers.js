b8fc18af48ba7599b57769e13237eb9b1f77e91a
dreak603 <dreak603@gmail.com>
2019-12-12 02:20:21 +0800

    fix Traivs.yaml err

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -5,20 +5,29 @@ const Image = db.Image;
 const Op = Sequelize.Op;
 
 const productController = {
-    getHomePageProducts: (req, res) => {
-        return Product.findAll().then(async products => {
-            const Images = await Image.findAll().then(images => images);
-            if (products) {
-                products = products.map(p => ({
-                    ...p.dataValues,
-                    Image: Images.filter(i => i.dataValues).find(item => item.ProductId == p.dataValues.id).url
-                }))
-                // console.log('products log', products);
-                return res.status(200).json({ status: 'success', products });
-            }
-            return res.status(400).json({ status: 'error', message: 'fail to find products' });
-        })
-    }
-}
+  getHomePageProducts: (req, res) => {
+    return Product.findAll().then(async products => {
+      const Images = await Image.findAll().then(images => images);
+      console.log('products log', Images);
+      if (products) {
+        products = products.map(p => ({
+          ...p.dataValues,
+          Image: Images.filter(i => i.dataValues).find(
+            item => item.ProductId == p.dataValues.id
+          )
+            ? Images.filter(i => i.dataValues).find(
+                item => item.ProductId == p.dataValues.id
+              ).url
+            : null
+        }));
+        console.log('products log', products);
+        return res.status(200).json({ status: 'success', products });
+      }
+      return res
+        .status(400)
+        .json({ status: 'error', message: 'fail to find products' });
+    });
+  }
+};
 
 module.exports = productController;
--- test/requests/products.spec.js
+++ test/requests/products.spec.js
@@ -9,32 +9,32 @@ const expect = chai.expect;
 const db = require('../../models');
 
 describe('# Product request', () => {
-    context('# Home Request', () => {
-        describe('When Visit HomePage', () => {
-            before(async () => {
-                await db.Product.create({ name: 'Product1 Test',  price: 3000 });
-                await db.Product.create({ name: 'Product2 Test',  price: 3000 });
-                await db.Image.create({ url: 'test1.jpg', ProductId: 1 });
-                await db.Image.create({ url: 'test2.jpg', ProductId: 2 });
-            })
+  context('# Home Request', () => {
+    describe('When Visit HomePage', () => {
+      before(async () => {
+        await db.Product.create({ name: 'Product1 Test', price: 3000 });
+        await db.Product.create({ name: 'Product2 Test', price: 3000 });
+        await db.Image.create({ url: 'test1.jpg', ProductId: 1 });
+        await db.Image.create({ url: 'test2.jpg', ProductId: 2 });
+      });
 
-            it('should return 200 and get json data', done => {
-                request(app)
-                    .get('/api/furnitures')
-                    .set('Accept', 'application/json')
-                    .expect(200)
-                    .end((err, res) => {
-                        if (err) return done(err);
-                        expect(res.body.status).to.equal('success');
-                        expect(res.body.products[0].name).to.equal('Product1 Test')
-                        done();
-                    })
-            })
+      it('should return 200 and get json data', done => {
+        request(app)
+          .get('/api/furnitures')
+          .set('Accept', 'application/json')
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.body.status).to.equal('success');
+            expect(res.body.products[0].name).to.equal('Product1 Test');
+            done();
+          });
+      });
 
-            after(async () => {
-                await db.Product.destroy({ where: {}, truncate: true });
-                await db.Image.destroy({ where: {}, truncate: true });
-            })
-        })
-    })
-})
+      after(async () => {
+        await db.Product.destroy({ where: {}, truncate: true });
+        await db.Image.destroy({ where: {}, truncate: true });
+      });
+    });
+  });
+});
