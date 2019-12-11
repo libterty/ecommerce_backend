2086d55006bf050b37e5a36ffa6e6e6c0064cf48
HuangMinShi <z7707092003@gmail.com>
2019-12-11 13:01:56 +0800

    Refix test/models require path

--- test/models/OrderItem.spec.js
+++ test/models/OrderItem.spec.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const should = chai.should();
 const db = require('../../models');
-const OrderItemModel = require('../../models/orderItem');
+const OrderItemModel = require('../../models/orderitem');
 const {
   sequelize,
   dataTypes,
