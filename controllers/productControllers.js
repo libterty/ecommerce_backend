2a56a8aad2e8c4616bbdaaef1d1f07fb2452d9cc
dreak603 <dreak603@gmail.com>
2019-12-12 01:47:17 +0800

    finish getHomePageProducts fn

---
+++ controllers/productControllers.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const db = require('../models');
+const Product = db.Product;
+const Image = db.Image;
+const Op = Sequelize.Op;
+
+const productController = {
+    getHomePageProducts: (req, res) => {
+        return Product.findAll().then(async products => {
+            const Images = await Image.findAll().then(images => images);
+            if (products) {
+                products = products.map(p => ({
+                    ...p.dataValues,
+                    Image: Images.filter(i => i.dataValues).find(item => item.ProductId == p.dataValues.id).url
+                }))
+                // console.log('products log', products);
+                return res.status(200).json({ status: 'success', products });
+            }
+            return res.status(400).json({ status: 'error', message: 'fail to find products' });
+        })
+    }
+}
+
+module.exports = productController;
--- routes/apis.js
+++ routes/apis.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const productController = require('../controllers/productControllers');
 
 router.get('/', (req, res) =>
   res.status(200).json({ status: 'success', message: 'Hello World!' })
 );
+
+router.get('/furnitures', productController.getHomePageProducts);
 
 module.exports = router;
