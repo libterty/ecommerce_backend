cc8e5dbee5c0199bbc988d4645d1dbe5febc7a88
dreak603 <dreak603@gmail.com>
2020-01-11 03:06:38 +0800

    add cache layer for products

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,11 +1,13 @@
 const Sequelize = require('sequelize');
 const db = require('../models');
+const Cache = require('../util/cache');
 const Product = db.Product;
 const Image = db.Image;
 const Color = db.Color;
 const Inventory = db.Inventory;
 const Category = db.Category;
 const Op = Sequelize.Op;
+const cache = new Cache();
 
 const productController = {
   /**
@@ -116,36 +118,75 @@ const productController = {
    *         200:
    *           description: success
    */
-  getProduct: (req, res) => {
-    return Product.findByPk(req.params.id).then(async product => {
-      if (product) {
-        let Images = await Image.findAll({
-          where: { ProductId: req.params.id }
-        }).then(images => images);
-        let Colors = await Color.findAll({
-          where: { ProductId: req.params.id }
-        }).then(colors => colors);
-        let Inventories = await Inventory.findAll({
-          where: { ProductId: req.params.id }
-        }).then(inventories => inventories);
-        Images = Images.map(image => ({ ...image.dataValues }));
-        Colors = Colors.map(color => ({
-          ...color.dataValues,
-          Inventory: Inventories.filter(i => i.dataValues).find(
-            item => item.id == color.id
-          )
-        }));
-        product.increment('viewCounts').then(product => {
+  getProduct: async (req, res) => {
+    const result = await cache.get(`getProduct${req.connection.remoteAddress}:${req.params.id}`);
+    if (result !== null) {
+      return Product.findByPk(req.params.id).then(async product => {
+        if (product) {
+          let Images = await Image.findAll({
+            where: { ProductId: req.params.id }
+          }).then(images => images);
+          let Colors = await Color.findAll({
+            where: { ProductId: req.params.id }
+          }).then(colors => colors);
+          let Inventories = await Inventory.findAll({
+            where: { ProductId: req.params.id }
+          }).then(inventories => inventories);
+          Images = Images.map(image => ({ ...image.dataValues }));
+          Colors = Colors.map(color => ({
+            ...color.dataValues,
+            Inventory: Inventories.filter(i => i.dataValues).find(
+              item => item.id == color.id
+            )
+          }));
+          product.increment('viewCounts').then(async product => {
+            await cache.set(`getProduct${req.connection.remoteAddress}:${req.params.id}`, { status: 'success', product, Images, Colors });
+            const newResult = await cache.get(`getProduct${req.connection.remoteAddress}:${req.params.id}`);
+            return res
+              .status(200)
+              .json(JSON.parse(newResult));
+          });
+        } else {
+          await cache.set(`getProduct${req.connection.remoteAddress}:${req.params.id}`, { status: 'error', message: 'Fail to find product' });
+          const newResult = await cache.get(`getProduct${req.connection.remoteAddress}:${req.params.id}`);
           return res
-            .status(200)
-            .json({ status: 'success', product, Images, Colors });
-        });
-      } else {
-        return res
-          .status(400)
-          .json({ status: 'error', message: 'Fail to find product' });
-      }
-    });
+            .status(400)
+            .json(JSON.parse(newResult));
+        }
+      });
+    } else {
+      return Product.findByPk(req.params.id).then(async product => {
+        if (product) {
+          let Images = await Image.findAll({
+            where: { ProductId: req.params.id }
+          }).then(images => images);
+          let Colors = await Color.findAll({
+            where: { ProductId: req.params.id }
+          }).then(colors => colors);
+          let Inventories = await Inventory.findAll({
+            where: { ProductId: req.params.id }
+          }).then(inventories => inventories);
+          Images = Images.map(image => ({ ...image.dataValues }));
+          Colors = Colors.map(color => ({
+            ...color.dataValues,
+            Inventory: Inventories.filter(i => i.dataValues).find(
+              item => item.id == color.id
+            )
+          }));
+          product.increment('viewCounts').then(async product => {
+            await cache.set(`getProduct${req.connection.remoteAddress}:${req.params.id}`, { status: 'success', product, Images, Colors });
+            return res
+              .status(200)
+              .json({ status: 'success', queue: 'First Request', product, Images, Colors });
+          });
+        } else {
+          await cache.set(`getProduct${req.connection.remoteAddress}:${req.params.id}`, { status: 'error', message: 'Fail to find product' });
+          return res
+            .status(400)
+            .json({ status: 'error', message: 'Fail to find product' });
+        }
+      });
+    }
   },
 
   /**
@@ -163,26 +204,51 @@ const productController = {
    *         200:
    *           description: success
    */
-  searchProducts: (req, res) => {
+  searchProducts: async (req, res) => {
     const search = decodeURI(req.query.items);
-
-    return Product.findAll({
-      include: [Category, Image],
-      limit: 10,
-      where: {
-        name: {
-          [Op.substring]: search
+    const result = await cache.get(`searchProducts${req.connection.remoteAddress}:${search}`);
+    if (result !== null) {
+      return Product.findAll({
+        include: [Category, Image],
+        limit: 10,
+        where: {
+          name: {
+            [Op.substring]: search
+          }
         }
-      }
-    }).then(products => {
-      if (products.length > 0) {
-        products = products.sort((a, b) => b.viewCounts - a.viewCounts);
-        return res.status(200).json({ status: 'success', products });
-      }
-      return res
-        .status(400)
-        .json({ status: 'error', message: 'Cannot find products' });
-    });
+      }).then(async products => {
+        if (products.length > 0) {
+          products = products.sort((a, b) => b.viewCounts - a.viewCounts);
+          await cache.set(`searchProducts${req.connection.remoteAddress}:${search}`, { status: 'success', products });
+          const newResult = await cache.get(`searchProducts${req.connection.remoteAddress}:${search}`);
+          return res.status(200).json(JSON.parse(newResult));
+        }
+        await cache.set(`searchProducts${req.connection.remoteAddress}:${search}`, { status: 'error', message: 'Cannot find products' });
+        return res
+          .status(400)
+          .json({ status: 'error', message: 'Cannot find products' });
+      });
+    } else {
+      return Product.findAll({
+        include: [Category, Image],
+        limit: 10,
+        where: {
+          name: {
+            [Op.substring]: search
+          }
+        }
+      }).then(async products => {
+        if (products.length > 0) {
+          products = products.sort((a, b) => b.viewCounts - a.viewCounts);
+          await cache.set(`searchProducts${req.connection.remoteAddress}:${search}`, { status: 'success', products });
+          return res.status(200).json({ status: 'success', queue: 'First Request', products });
+        }
+        await cache.set(`searchProducts${req.connection.remoteAddress}:${search}`, { status: 'error', message: 'Cannot find products' });
+        return res
+          .status(400)
+          .json({ status: 'error', queue: 'First Request', message: 'Cannot find products' });
+      });
+    }
   }
 };
 
