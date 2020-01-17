069347db0cb4fcebc13a155b6494220c93f7c47d
dreak603 <dreak603@gmail.com>
2020-01-17 15:40:36 +0800

    Revert "change port to 80"
    This reverts commit 7f850d6321a0d347b96673c642ffcf046fc8d9d3.

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY package.json ./
 RUN npm install
 RUN npm install pm2 -g
 COPY . .
-EXPOSE 80
+EXPOSE 3000
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const nosqlConnect = require('./nosql/index');
 const storeLogs = require('./middlewares/index');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
